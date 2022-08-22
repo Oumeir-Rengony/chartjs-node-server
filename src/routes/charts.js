38aff487cdf2a61a6b3aa66454682c003bd1a616
Mohammad Oumeir Rengony <oumeir.rengony@rapp.com>
2022-08-22 20:24:39 +0400

    update response format

--- src/routes/charts.js
+++ src/routes/charts.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const ChartController = require('../controllers/ChartController');
 
 router.get('/ping', async (req,res) => {
-    const response = await new ChartController().pingServer();
+    const data = await new ChartController().pingServer();
+    const response = { data };
     res.send(response);
 });
 
 
 router.post('/createChart', async (req,res) => {
-    const response = await new ChartController().createChart(req.body);
+    const data = await new ChartController().createChart(req.body);
+    const response = { data };
     res.send(response);
 });
 
